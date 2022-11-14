e6c8f8826d5071b75e3a264a563092f5db55b64d
Corentin Cuvelier <81868176+Corentin1501@users.noreply.github.com>
2022-11-14 10:52:45 +0100

    Delete Projet directory

--- Projet/expressions/constante.cc
+++
@@ -1,7 +0,0 @@
-#include "constante.hh"
-
-Constante::Constante(double valeur) : _valeur(valeur) {}
-
-double Constante::calculer(const Contexte & contexte) const {
-    return _valeur;
-}
--- Projet/expressions/variable.cc
+++
@@ -1,7 +0,0 @@
-#include "variable.hh"
-
-Variable::Variable(const std::string& nom) : _nom(nom) {}
-
-double Variable::calculer(const Contexte & contexte) const {
-    return contexte[_nom];
-}
